c6642014ce894e8dfa58d294acd6a7fb9e4de970
W.G. Kasun Dimantha <kasundimantha97@gmail.com>
2024-01-31 16:34:28 +0530

    Create CustomerDaoImpl

---
+++ JavaApplication9/src/edu/layered/dao/custom/impl/CustomerDaoImpl.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.layered.dao.custom.impl;
+
+import edu.layered.dao.custom.CustomerDao;
+import edu.layered.entity.CustomerEntity;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tharidu dilshan
+ */
+public class CustomerDaoImpl implements CustomerDao{
+
+    @Override
+    public boolean add(CustomerEntity t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(CustomerEntity t) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public CustomerEntity get(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<CustomerEntity> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
